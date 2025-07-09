1fdb20887fcc69f49cca962c5248c0e8112c22e2
novaardiansyah <novaardiansyah78@gmail.com>
2025-07-09 21:37:38 +0700

    Update .env.example to separate APP_NAME and APP_AUTHOR details; add ScheduledPaymentJob for handling scheduled payments in the console schedule.

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Jobs\Payment\ScheduledPaymentJob;
+use App\Jobs\PaymentResource\ScheduledPaymentJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;

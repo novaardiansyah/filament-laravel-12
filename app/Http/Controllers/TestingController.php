b85b740574c85f5fe734f2a1e25b4eb864507452
novaardiansyah <novaardiansyah78@gmail.com>
2025-07-20 02:22:33 +0700

    Add ContactMessage model, migration, and ReplyContactMail for email responses

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -2,44 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Payment;
-use App\Models\User;
+use App\Mail\ContactMessageResource\ReplyContactMail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Mail;
 
 class TestingController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    $now = now()->translatedFormat('d/m/Y H:i');
-    
-    $mpdf     = new \Mpdf\Mpdf();
-    $rowIndex = 1;
-    $periode  = now()->subDays(2)->toDateString();
-    $user     = auth()->user() ?? User::find(1); // ! Default user if not authenticated
+    $preview = (bool) $request->input('preview', 0);
 
-    $mpdf->WriteHTML(view('payment-resource.make-pdf.header', [
-      'title'   => 'Laporan keuangan harian',
-      'now'     => $now,
-      'periode' => carbonTranslatedFormat($periode, 'l, d F Y'),
-      'user'    => $user,
-    ])->render());
-    
-    Payment::where([
-      'date' => $periode,
-    ])->chunk(200, function ($list) use ($mpdf, &$rowIndex) {
-      foreach ($list as $record) {
-        $view = view('payment-resource.make-pdf.body', [
-          'record'    => $record,
-          'loopIndex' => $rowIndex++,
-        ])->render();
+    $data = [
+      'name'    => textCapitalize('John Doe'),
+      'email'   => 'novaardiansyah78@gmail.com',
+      'subject' => 'Terima Kasih Telah Menghubungi Saya',
+    ];
 
-        $mpdf->WriteHTML($view);
-      }
-    });
+    if (!$preview) {
+      Mail::to($data['email'])->queue(new ReplyContactMail($data));
+      echo 'Email has been queued for sending.';
+    }
 
-    $result = makePdf($mpdf, 'daily-payment-report', $user, true);
-
-    return $result;
+    $process = new ReplyContactMail($data);
+    return $process->render();
   }
 }
---
+++ app/Mail/ContactMessageResource/ReplyContactMail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail\ContactMessageResource;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+
+class ReplyContactMail extends Mailable implements ShouldQueue
+{
+  use Queueable, SerializesModels;
+
+  /**
+   * Create a new message instance.
+   */
+  public function __construct(public array $data = [])
+  {
+    //
+  }
+
+  /**
+   * Get the message envelope.
+   */
+  public function envelope(): Envelope
+  {
+    return new Envelope(
+      subject: $this->data['subject'],
+      replyTo: [
+        new Address(config('app.author_email'), config('app.author_name')),
+      ]
+    );
+  }
+
+  /**
+   * Get the message content definition.
+   */
+  public function content(): Content
+  {
+    // * view('mails.contact-message-resource.reply-contact-mail')
+    return new Content(
+      view: 'mails.contact-message-resource.reply-contact-mail',
+    );
+  }
+
+  /**
+   * Get the attachments for the message.
+   *
+   * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+   */
+  public function attachments(): array
+  {
+    return [];
+  }
+}

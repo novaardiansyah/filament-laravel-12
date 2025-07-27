b534d601cd13cb0de556266761e41f098b63c490
novaardiansyah <novaardiansyah78@gmail.com>
2025-07-27 22:21:40 +0700

    Refactor LogUserLogin listener: Update IP logging logic to use ActivityLog and streamline email notification process.

--- app/Listeners/LogUserLogin.php
+++ app/Listeners/LogUserLogin.php
@@ -3,11 +3,11 @@
 namespace App\Listeners;
 
 use App\Mail\UserResource\NotifUserLoginMail;
+use App\Models\ActivityLog;
 use App\Models\EmailLog;
-use App\Models\UserLog;
+use App\Models\Setting;
+use App\Models\User;
 use Illuminate\Auth\Events\Login;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
@@ -36,16 +36,20 @@ class LogUserLogin
     
     $ip_address = request()->ip();
 
-    // ! Cek di UserLog dengan IP, jika dalam 6 jam sudah ada, tidak perlu kirim email lagi
-    $existingLog = UserLog::where('ip_address', $ip_address)
-      ->where('created_at', '>=', now()->subHours(6))
-      ->first();
-    
-    if ($existingLog) {
-      return;
-    }
+    // ! Cek di ActivityLog dengan IP, jika sudah ada, tidak perlu kirim email lagi
+    $interval = Setting::where('key', 'interval_login_notification')->first()?->value ?? '24 Jam';
+    $interval = (int) preg_replace('/\D/', '', $interval);
 
-    $ip_info = Http::get("https://ipinfo.io/{$ip_address}/json?token=" . config(key: 'services.ipinfo.token'))->json();
+    $existingLog = ActivityLog::where('ip_address', $ip_address)
+      ->where('event', 'Login Notification')
+      ->where('created_at', '>=', now()->subHours($interval))
+      ->first();
+
+    if ($existingLog) return;
+
+    // ! Ambil IP pertama jika ada beberapa
+    $ip_address = explode(',', $ip_address)[0];
+    $ip_info    = Http::get("https://ipinfo.io/{$ip_address}/json?token=" . config(key: 'services.ipinfo.token'))->json();
 
     $country = $ip_info['country'] ?? null;
     $city    = $ip_info['city'] ?? null;
@@ -63,33 +67,41 @@ class LogUserLogin
     $timezone = $ip_info['timezone'] ?? null;
     $now      = now();
 
-    $saveLog = UserLog::create([
-      'user_id'    => $event->user->id,
-      'email'      => $event->user->email,
-      'ip_address' => $ip_address,
-      'country'    => $country,
-      'city'       => $city,
-      'region'     => $region,
-      'postal'     => $postal,
-      'geolocation'=> $geolocation,
-      'timezone'   => $timezone,
-      'user_agent' => request()->headers->get('user-agent'),
-      'referer'    => url('admin/login'),
-      'created_at' => $now,
-      'updated_at' => $now,
-    ]);
+    $log = [
+      'log_name'    => 'Notification',
+      'description' => 'Email Login Notification Sent',
+      'event'       => 'Login Notification',
+      'causer_type' => User::class,
+      'causer_id'   => $event->user->id,
+      'ip_address'  => $ip_address,
+      'country'     => $country,
+      'city'        => $city,
+      'region'      => $region,
+      'postal'      => $postal,
+      'geolocation' => $geolocation,
+      'timezone'    => $timezone,
+      'user_agent'  => request()->userAgent(),
+      'referer'     => url('admin/login'),
+      'properties'  => [
+        'email_user' => $event->user->email,
+      ],
+    ];
 
-    $data = array_merge(
-      $saveLog->toArray(),
-        [
-        'log_name'   => 'notif_user_login',
-        'email_user' => $saveLog->email,
-        'email'      => config('app.author_email'),
-        'subject'    => 'Notifikasi: Login pengguna dari situs web',
-        'address'    => $address,
-        'created_at' => $now,
-      ]
-    );
+    ActivityLog::create($log);
+
+    $data = [
+      'email'       => config('app.author_email'),
+      'log_name'    => 'notif_user_login',
+      'subject'     => 'Notifikasi: Login pengguna dari situs web',
+      'email_user'  => $event->user->email,
+      'ip_address'  => $log['ip_address'],
+      'geolocation' => $log['geolocation'],
+      'user_agent'  => $log['user_agent'],
+      'timezone'    => $log['timezone'],
+      'referer'     => $log['referer'],
+      'address'     => $address,
+      'created_at'  => $now,
+    ];
 
     $mailObj = new NotifUserLoginMail($data);
     $message = $mailObj->render();

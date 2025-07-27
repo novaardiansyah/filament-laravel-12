30f4a755fb02677dc927ded4e549fd8f50446562
novaardiansyah <novaardiansyah78@gmail.com>
2025-07-27 22:27:29 +0700

    Remove UserLog resource and related files: Drop UserLog model, policy, and associated pages from the application.

--- app/Policies/ActivityLogPolicy.php
+++ app/Policies/ActivityLogPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\UserLog;
+use App\Models\ActivityLog;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class UserLogPolicy
+class ActivityLogPolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,15 @@ class UserLogPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_user::log');
+        return $user->can('view_any_activity::log');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, UserLog $userLog): bool
+    public function view(User $user, ActivityLog $activityLog): bool
     {
-        return $user->can('view_user::log');
+        return $user->can('view_activity::log');
     }
 
     /**
@@ -31,23 +31,23 @@ class UserLogPolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_user::log');
+        return $user->can('create_activity::log');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, UserLog $userLog): bool
+    public function update(User $user, ActivityLog $activityLog): bool
     {
-        return $user->can('update_user::log');
+        return $user->can('update_activity::log');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, UserLog $userLog): bool
+    public function delete(User $user, ActivityLog $activityLog): bool
     {
-        return $user->can('delete_user::log');
+        return $user->can('delete_activity::log');
     }
 
     /**
@@ -55,15 +55,15 @@ class UserLogPolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_user::log');
+        return $user->can('delete_any_activity::log');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, UserLog $userLog): bool
+    public function forceDelete(User $user, ActivityLog $activityLog): bool
     {
-        return $user->can('force_delete_user::log');
+        return $user->can('force_delete_activity::log');
     }
 
     /**
@@ -71,15 +71,15 @@ class UserLogPolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('force_delete_any_user::log');
+        return $user->can('force_delete_any_activity::log');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, UserLog $userLog): bool
+    public function restore(User $user, ActivityLog $activityLog): bool
     {
-        return $user->can('restore_user::log');
+        return $user->can('restore_activity::log');
     }
 
     /**
@@ -87,15 +87,15 @@ class UserLogPolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('restore_any_user::log');
+        return $user->can('restore_any_activity::log');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, UserLog $userLog): bool
+    public function replicate(User $user, ActivityLog $activityLog): bool
     {
-        return $user->can('replicate_user::log');
+        return $user->can('replicate_activity::log');
     }
 
     /**
@@ -103,6 +103,6 @@ class UserLogPolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('reorder_user::log');
+        return $user->can('reorder_activity::log');
     }
 }

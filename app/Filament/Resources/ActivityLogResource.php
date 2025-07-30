c7f203b0a73541299f55fd13d7bc1fcf2a7c7dc1
novaardiansyah <novaardiansyah78@gmail.com>
2025-07-30 12:46:10 +0700

    Refactor ActivityLogResource and ActivityLogTable: Remove ID column and filter, update record URL for better action handling

--- app/Filament/Resources/ActivityLogResource.php
+++ app/Filament/Resources/ActivityLogResource.php
@@ -202,9 +202,6 @@ class ActivityLogResource extends Resource
         Tables\Columns\TextColumn::make('index')
           ->rowIndex()
           ->label('#'),
-        Tables\Columns\TextColumn::make('id')
-          ->label('ID Aktivitas')
-          ->toggleable(isToggledHiddenByDefault: true),
         Tables\Columns\TextColumn::make('log_name')
           ->badge()
           ->colors(static::getLogNameColors())
@@ -244,20 +241,6 @@ class ActivityLogResource extends Resource
       ->recordUrl(null)
       ->defaultSort('created_at', 'desc')
       ->filters([
-        Filters\Filter::make('id')
-          ->form([
-            Forms\Components\TextInput::make('id')
-              ->label('ID Aktivitas')
-              ->numeric(),
-          ])
-          ->query(function (Builder $query, array $data): Builder {
-            return $query->when($data['id'], fn(Builder $query, $id) => $query->where('id', $id));
-          })
-          ->indicateUsing(function (array $data): ?string {
-            if (!$data['id']) return null;
-            return 'ID Aktivitas: ' . $data['id'];
-          }),
-
         Filters\SelectFilter::make('log_name')
           ->label(__('filament-logger::filament-logger.resource.label.type'))
           ->options(static::getLogNameList()),
